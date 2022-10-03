f325dc6dfe2b7930e34373c1701c954dab0ce960
abby <diya811125@gmail.com>
2022-10-03 22:26:06 +0800

    16

--- js/index6/index6.js
+++ js/index6/index6.js
@@ -235,9 +235,9 @@ function doOnOrientationChange() {
           document.querySelector("#video_iframe_1").style.height = height + "px";
           document.querySelector("#video_iframe_2").style.height = height + "px";
           document.querySelector("#video_iframe_3").style.height = height + "px";
-          // document.querySelector("#video_iframe_3").style.width = (width - 100) + "px";
           width = $(window).width();
           document.querySelector("#width").innerHTML = width;
+          document.querySelector("#video_iframe_3").style.width = (width - 100) + "px";
         });
         window.addEventListener('scroll', function () {
           let nowHeight = window.innerHeight;
@@ -263,9 +263,9 @@ function doOnOrientationChange() {
           document.querySelector("#video_iframe_1").style.height = "calc(100vh - 250px)";
           document.querySelector("#video_iframe_2").style.height = "calc(100vh - 250px)";
           document.querySelector("#video_iframe_3").style.height = "calc(100vh - 250px)";
-          // document.querySelector("#video_iframe_3").style.width = width + "px";
           width = $(window).width();
           document.querySelector("#width").innerHTML = width;
+          document.querySelector("#video_iframe_3").style.width = width + "px";
         });
         window.addEventListener('scroll', function () {
           document.querySelector("#video_iframe_1").style.height = "calc(100vh - 250px)";
