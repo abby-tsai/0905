c0abdd4e322f3fa5603114968bc315ffea754ec6
abby <diya811125@gmail.com>
2022-10-05 14:47:02 +0800

    03

--- js/index6/index_5.js
+++ js/index6/index_5.js
@@ -86,7 +86,7 @@ function slider(wrapper, items, prev, next, slidesLi, dots) {
 
   function dragStart(e) {
     e = e || window.event;
-    // e.preventDefault();
+    e.preventDefault();
     posInitial = items.offsetLeft;
 
     if (e.type == 'touchstart') {
